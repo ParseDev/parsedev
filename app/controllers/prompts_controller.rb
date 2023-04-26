7def24ed9dcbbf87d714e0e22dce0d4f803d99e9
Elie Toubiana <elie.toubiana@gmail.com>
2023-04-26 07:33:46 -0700

    better formattting + better views

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -14,39 +14,39 @@ class PromptsController < ApplicationController
       username: datasource.database_username,
       password: datasource.database_password
     }
-
-
+    
+    
     owner_name = 'ChartGPT'
-env_name = Rails.env
-spec_name = 'primary'
-database_configurations = ActiveRecord::DatabaseConfigurations.new(
-  env_name => { spec_name => db_config_hash }
-)
-
-# Retrieve the specific database configuration for the given environment and specification name
-db_config = database_configurations.configs_for(env_name: env_name, name: spec_name)
-
-# Define the connection class, role, and shard
-connection_class = ActiveRecord::Base
-role = :reading
-shard = :default
-
-# Create a PoolConfig object from the database configuration
-pool_config = ActiveRecord::ConnectionAdapters::PoolConfig.new(connection_class, db_config, role, shard)
-
-# Instantiate the ConnectionPool object with the PoolConfig object
-connection_pool = ActiveRecord::ConnectionAdapters::ConnectionPool.new(pool_config)
-
-# You can now use the connection pool to check out connections to the database
-connection = connection_pool.checkout
-# Perform database operations with the connection
-# ...
-# Check the connection back into the pool when done
-connection_pool.checkin(connection)
+    env_name = Rails.env
+    spec_name = 'primary'
+    database_configurations = ActiveRecord::DatabaseConfigurations.new(
+      env_name => { spec_name => db_config_hash }
+    )
+    
+    # Retrieve the specific database configuration for the given environment and specification name
+    db_config = database_configurations.configs_for(env_name: env_name, name: spec_name)
+    
+    # Define the connection class, role, and shard
+    connection_class = ActiveRecord::Base
+    role = :reading
+    shard = :default
+    
+    # Create a PoolConfig object from the database configuration
+    pool_config = ActiveRecord::ConnectionAdapters::PoolConfig.new(connection_class, db_config, role, shard)
+    
+    # Instantiate the ConnectionPool object with the PoolConfig object
+    connection_pool = ActiveRecord::ConnectionAdapters::ConnectionPool.new(pool_config)
+    
+    # You can now use the connection pool to check out connections to the database
+    connection = connection_pool.checkout
+    # Perform database operations with the connection
+    # ...
+    # Check the connection back into the pool when done
+    connection_pool.checkin(connection)
     
     
     sql = Boxcars::SQL.new(engine: engine, connection: connection)
-
+    
     result = sql.conduct(params[:input_field])
     puts "Result: #{result}}"
     respond_to do |format|
@@ -54,7 +54,7 @@ connection_pool.checkin(connection)
       format.html { redirect_to prompt_path }
     end
   end
-
+  
   def show
   end
 end
