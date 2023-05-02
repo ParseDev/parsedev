229a45cb7da894e485208bb7cbac261ed1d616d7
Elie Toubiana <elie.toubiana@gmail.com>
2023-05-01 20:46:50 -0700

    Better style

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -51,9 +51,8 @@ class PromptsController < ApplicationController
       boxcar = Boxcars::Swagger.new(engine: engine, swagger_url: 'https://raw.githubusercontent.com/stripe/openapi/master/openapi/spec3.yaml',  context: "API_token: #{datasource.stripe_token}")
     end
     result = boxcar.conduct(params[:input_field])
-    puts "Result: #{result}}"
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.update('result_frame', partial: 'result', locals: { result: result }) }
+      format.turbo_stream { render turbo_stream: turbo_stream.append('result_frame', partial: 'result', locals: { result: result }) }
       format.html { redirect_to prompt_path }
     end
   end
