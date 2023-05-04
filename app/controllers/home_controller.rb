c069bf1d5829c80e134e9def3c9f952f77b43f26
Elie Toubiana <elie.toubiana@gmail.com>
2023-05-04 13:46:54 -0700

    Fix deploy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
   def index
-    if user_signed_in?
-      redirect_to new_chat_path
-    end
-    @display_chat = true
   end
 end
