a7a027f59ae12b8929ab2547e7f69ad06f9fc8c7
Elie Toubiana <elie.toubiana@gmail.com>
2023-05-11 09:26:09 -0700

    Clean up

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,6 +2,10 @@ class ChatsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @display_chat = true
+    if current_user.company.datasources.count == 0
+      redirect_to new_datasource_path, notice: "Please connect a datasource to start chatting."
+    else
+      @display_chat = true
+    end
   end
 end
