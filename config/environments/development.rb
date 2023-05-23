4f7781ba2b8d591d62a200d9ee8128d671f6a53a
qubitam <qubitam@gmail.com>
2023-05-23 19:27:34 +0100

    add salt keys for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-  config.active_record.encryption.support_unencrypted_data = true
 
 end
