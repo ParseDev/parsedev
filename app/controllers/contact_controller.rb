0ae6f34d429c83a9391c0d569e6395232c8a5813
Elie Toubiana <elie.toubiana@gmail.com>
2023-05-30 23:37:55 -0700

    Update contact_controller.rb

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,7 +6,7 @@ class ContactController < ApplicationController
     contact = Contact.new(contact_params)
     if contact.save
       flash[:notice] = "Thank you for your interest in ParseDev! We will be in touch soon."
-      redirect_to beta_path
+      redirect_to "https://calendly.com/elie-parse/30min"
     else
       flash[:alert] = "There was a problem submitting your request. Please try again."
       redirect_to beta_path
