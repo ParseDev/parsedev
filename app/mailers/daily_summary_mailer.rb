e3144c68b423416a6ac53dbd9a83a4f03f168266
Elie Toubiana <elie.toubiana@gmail.com>
2023-06-11 15:31:23 -0700

    Adding more details to daily email

--- app/mailers/daily_summary_mailer.rb
+++ app/mailers/daily_summary_mailer.rb
@@ -2,8 +2,8 @@ class DailySummaryMailer < ApplicationMailer
   default to: "elie@parse.dev", subject: "Parse Daily Summary"
 
   def summary_email()
-    @new_signups = User.where("created_at >= ?", 1.day.ago).count
-    @new_prompts = Prompt.where("created_at >= ?", 1.day.ago).count
+    @new_signups = User.where("created_at >= ?", 1.day.ago)
+    @new_prompts = Prompt.where("created_at >= ?", 1.day.ago)
     mail
   end
 end
