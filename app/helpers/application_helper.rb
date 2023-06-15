c9d5f97ab4a798ccdf368b33793586cc914265fd
Elie Toubiana <elie.toubiana@gmail.com>
2023-06-15 07:22:37 -0700

    attempt to fix chat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   def flash_class(type)
     case type.to_sym
     when :notice
-      "bg-green-500 text-white px-4 py-2 rounded"
+      "bg-green-500 text-white px-4 py-4 rounded m-2"
     when :alert
-      "bg-red-500 text-white px-4 py-2 rounded"
+      "bg-red-500 text-white px-4 py-4 rounded m-2"
     else
-      "bg-blue-500 text-white px-4 py-2 rounded"
+      "bg-blue-500 text-white px-4 py-4 rounded m-2"
     end
   end
 
